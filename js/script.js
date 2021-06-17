0337940440f33ff948604ce2cf6b46d9cfadd432
blowupmeisei <blowupmeisei@gmail.com>
2021-06-17 18:33:59 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('#slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 5000,
+    arrows: false,
+    centerPadding: '100px',
+    centerMode: true,
+
+  });
+});
